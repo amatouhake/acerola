61db74020f93043866c1fc0c274a82f34112569f
amatouhake <kenkenhagizou@gmail.com>
2021-02-28 03:04:19 +0900

    -

--- acerola/src/conversion.js
+++ acerola/src/conversion.js
@@ -3,24 +3,15 @@ const fs = require('fs');
 console.log(conversion('public/'));
 
 function conversion(d) {
-    return `fs.mkdirSync('${d.slice(0,d.length-1)}');`+fs.readdirSync(d).forEach(f => {
+    console.log(`fs.mkdirSync('${d.slice(0,d.length-1)}');`);
+    fs.readdirSync(d).forEach(f => {
         let p = d+f;
-        let s = fs.statSync(p);
-        if(s.isDirectory()) {
-            console.log(p);
+        let stat = fs.statSync(p);
+        if(stat.isDirectory()) {
             conversion(`${p}/`);
         }
-    });
-
-
-    fs.readdirSync(p).forEach(file => {
-        let f = p+file;
-        let stat = fs.statSync(f);
-        if(stat.isDirectory()) {
-            return `fs.mkdirSync('${p.slice(0,p.length-1)}');`//+conversion(`${f}/`);
-        } else {
-            return;
-            console.log(`fs.writeFileSync('${f.slice(3,f.length)}', \`${fs.readFileSync(f, 'utf8').replace(/\r^\s*/gm, '').replace(/`/g, '\\`').replace(/\$/g, '\\$')}\`, 'utf8');`);
+        if(stat.isFile()) {
+            console.log(`fs.writeFileSync('${p}', \`${fs.readFileSync(p, 'utf8').replace(/\r^\s*/gm, '').replace(/`/g, '\\`').replace(/\$/g, '\\$')}\`, 'utf8');`);
         }
     });
 }
--- terminal.bat
+++ terminal.bat
@@ -11,4 +11,4 @@ if not exist %node% (
 )
 
 cd acerola
-cmd
+cmd /k
