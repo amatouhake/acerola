722daad226d4281bb9ff731c27ced9289f8b1e2c
amatouhake <kenkenhagizou@gmail.com>
2021-03-03 06:53:35 +0900

    -

--- acerola/src/public/danmaku/danmaku.js
+++ acerola/src/public/danmaku/danmaku.js
@@ -1,74 +1,105 @@
-let c = document.createElement("canvas");
-document.body.appendChild(c);
-let ctx = c.getContext("2d");
-let t, m;
-let w = 900;
-let h = 1600;
-let k = new Set();
+var config = {
+    type: Phaser.AUTO,
+    width: 450,
+    height: 800,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: false
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
 
-resize();
-addEventListener('resize', resize);
+var game = new Phaser.Game(config);
 
-addEventListener('mousedown', e => {
-    k.add("mouse");
-});
+var cursors;
+var player;
+var speed = 1;
+var time = 0;
+var shot;
+var shots;
+var turbo;
+var turbo_time = 0;
+var turbo_delay = 0;
 
-addEventListener('mouseup', e => {
-    k.delete("mouse");
-});
+function preload () {
+    this.load.image('space', 'assets/space.png');
+    this.load.image('shot', 'assets/shot.png');
+    this.load.spritesheet('player', 'assets/player.png', { frameWidth: 48, frameHeight: 48 });
+}
 
-addEventListener('keydown', e => {
-    k.add(e.key);
-});
+function create () {
+    this.add.image(225, 400, 'space');
+    player = this.physics.add.sprite(225, 600, 'player');
 
-addEventListener('keyup', e => {
-    k.delete(e.key);
-});
+    player.setCollideWorldBounds(true);
+    shots = this.physics.add.group();
 
-function resize() {
-    if(innerWidth/9<innerHeight/16) {
-        c.width = innerWidth;
-        c.height = innerWidth/9*16;
+    this.anims.create({
+        key: 'normal',
+        frames: this.anims.generateFrameNumbers('player', { start: 0, end: 3 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    this.anims.create({
+        key: 'turbo',
+        frames: this.anims.generateFrameNumbers('player', { start: 4, end: 7 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    player.anims.play('normal', true);
+
+    cursors = this.input.keyboard.createCursorKeys();
+    shot = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+    turbo = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SHIFT);
+}
+
+function goodbye(obj) { obj.kill();  obj.destroy();}
+
+function update () {
+    time += 1;
+    if(shot.isDown) {
+        if(time%5 == 0) player_shot();
+    }
+
+    if(turbo.isDown && turbo_time <= 100) {
+        player.anims.play('turbo', true);
+        speed = 2;
+        turbo_time += 1;
     } else {
-        c.width = innerHeight/16*9;
-        c.height = innerHeight;
+        player.anims.play('normal', true);
+        speed = 1;
+        if(turbo_time > 100) {
+            turbo_delay += 1;
+            if(turbo_delay > 400) {
+                turbo_time = 0;
+                turbo_delay = 0;
+            }
+        }
     }
-    m = c.width/w;
+
+    if (cursors.left.isDown) {
+        player.setVelocityX(-160*speed);
+    } else if (cursors.right.isDown) {
+        player.setVelocityX(160*speed);
+    } else if (cursors.up.isDown) {
+        player.setVelocityY(-160*speed);
+    } else if (cursors.down.isDown) {
+        player.setVelocityY(160*speed);
+    } else {
+        player.setVelocityX(0);
+        player.setVelocityY(0)
+    }
 }
 
-let p = new function() {
-    this.x = 450;
-    this.y = 1400;
-    this.hp = 20;
+function player_shot() {
+    var shot = shots.create(player.x, player.y, 'shot');
+    shot.setVelocityY(-320);
 }
-
-function loop() {
-    t += 1;
-
-    if(k.has("w")) {
-        p.y -= p.y > 0 ? 5 : 0;
-    }
-    if(k.has("s")) {
-        p.y += p.y < h ? 5 : 0;
-    }
-    if(k.has("a")) {
-        p.x -= p.x > 0 ? 5 : 0;
-    }
-    if(k.has("d")) {
-        p.x += p.x < w ? 5 : 0;
-    }
-
-    draw();
-    requestAnimationFrame(loop);
-}
-
-function draw() {
-    ctx.fillStyle = "black";
-    ctx.fillRect(0, 0, 900*m, c.height);
-    ctx.fillStyle = "white";
-    ctx.beginPath();
-    ctx.arc(p.x*m, p.y*m, 20*m, 0, Math.PI * 2, true);
-    ctx.fill();
-}
-
-loop();
