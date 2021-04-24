f9a7ca86d1dbf411ad51397244f5c0ffcf9fcfef
amatouhake <kenkenhagizou@gmail.com>
2021-04-25 04:21:36 +0900

    -

--- src/client/Main.tsx
+++ src/client/Main.tsx
@@ -1,1 +1,21 @@
-import react from "react";
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import "./style/Reset";
+import "./style/Main";
+import Header from "./script/Header";
+
+class Layout extends React.Component {
+  render() {
+    return (
+      <Router>
+        <Header />
+        <Link to="/">Home</Link>
+        <Link to="/About">About</Link>
+        <Route exact path='/' component={Header}/>
+      </Router>
+    );
+  }
+}
+
+ReactDOM.render(<Layout/>, document.body);
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,29 @@
-const debug   = process.env.NODE_ENV !== "production";
-const webpack = require("webpack");
+//const webpack = require("webpack");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-  context: __dirname,
-  entry: "./src/client/Main.js",
-  module: {
-    rules: [{
-      test: /\.tsx?$/,
+module.exports = (env, options) => {
+  let plugins = [
+    new HtmlWebpackPlugin({
+      template: `${__dirname}/src/client/index.html`,
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'acerola.min.css',
+    })
+  ];
+
+  return {
+    entry: "./src/client/Main.tsx",
+    output: {
+      path: `${__dirname}/public`,
+      filename: "acerola.min.js"
+    },
+    resolve: {
+      extensions: [ ".scss", ".css", ".tsx", ".ts", ".jsx", "..." ]
+    },
+    module: { rules: [
+      {
+        test: /\.tsx?$/,
         exclude: /(node_modules|bower_components)/,
         use: [
           {
@@ -21,20 +38,67 @@ module.exports = {
                   "useBuiltIns": "usage",
                   "corejs": 3
                 }],
-                "@babel/preset-react",
-                "@babel/preset-typescript"
+                "@babel/preset-react"
               ]
             }
-          }
+          },
+          {
+            loader: "ts-loader",
+            options: {
+              transpileOnly: true,
+              configFile: "webpack.tsconfig.json",
+            },
+          },
         ]
-      }]
-    },
-    output: {
-      path: `__dirname/${public}`,
-      filename: "client.min.js"
-    },
-    plugins: debug ? [] : [
-      new webpack.optimize.OccurrenceOrderPlugin(),
-      new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
-    ]
+      },
+      
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: "babel-loader",
+        options: {
+          presets: [
+            [ "@babel/preset-env", {
+              "targets": {
+                "edge": 44,
+                "ie": 11
+              },
+              "useBuiltIns": "usage",
+              "corejs": 3
+            }],
+            "@babel/preset-react"
+          ]
+        }
+      },
+      {
+        test: /\.html$/,
+        loader: "html-loader"
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [
+                  [ "autoprefixer", {
+                    grid: true,
+                    browsersList: [
+                      "edge >= 44",
+                      "ie >= 11"
+                    ]
+                  }],
+                ],
+              },
+            },
+          },
+          "sass-loader"
+        ]
+      },
+    ]},
+    plugins
+  };
 };
